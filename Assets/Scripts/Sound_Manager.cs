233767d896181ca7e256b744542b3ca6b592c602
RIBOUT <pifi77@hotmail.fr>
2019-10-19 17:56:08 +0200

    [UPDATE] Test event scene with the start event

--- Assets/Scripts/Sound_Manager.cs
+++ Assets/Scripts/Sound_Manager.cs
@@ -15,18 +15,39 @@ public class Sound_Manager : MonoBehaviour
     }
 
     public AK.Wwise.Event testEvent;
+    public AK.Wwise.Event startEvent;
+
+
+    [Header("A little surprise")]
+    public Animator ani;
 
     // Start is called before the first frame update
     void Start()
     {
+        AkSoundEngine.PostEvent(startEvent.Id, this.gameObject);
 
+        startEvent.Post(this.gameObject, (uint)AkCallbackType.AK_MusicSyncBeat, CallBackFunction);
     }
+
+    private void CallBackFunction(object baseObject, AkCallbackType type, object info)
+    {
+        Beat();
+    }
+
+    [ContextMenu("Beat")]
+    public void Beat()
+    {
+        ani.SetTrigger("Beat");
+        float ran = Random.Range(0f, 1f);
+        foreach (SpriteRenderer sR in ani.GetComponentsInChildren<SpriteRenderer>())
+            sR.color = Color.HSVToRGB(ran, 1, 1);
+    }
+
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Call the event " + testEvent.Id);
             AkSoundEngine.PostEvent(testEvent.Id, this.gameObject);
             Debug.Log("Call the event " + testEvent.Id);
         }
