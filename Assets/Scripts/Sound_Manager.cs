68a60912aff53f76234bd12afdda43aaac2a4c9b
RIBOUT <pifi77@hotmail.fr>
2019-10-19 11:07:40 +0200

    [ADD] A sound manager empty

---
+++ Assets/Scripts/Sound_Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Manager : MonoBehaviour
+{
+    public static Sound_Manager instance;
+
+    public void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Debug.LogError("Already the Sound_Manager");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+}
